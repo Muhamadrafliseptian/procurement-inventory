1dd604ce6cd755f441e064e96b6c52edbec8691a
sausanrafila <rafila235@gmail.com>
2023-06-21 21:33:59 +0700

    ubah deploy

--- deploy.mjs
+++ deploy.mjs
@@ -12,7 +12,7 @@ import fs from 'node:fs';
     await execa('git', ['checkout', '--orphan', targetBranch]);
     // eslint-disable-next-line no-console
     console.log('Building started...');
-    await execa('pnpm', ['build']);
+    await execa('npm', ['run', 'build']);
     // Understand if it's dist or build folder
     const folderName = fs.existsSync('dist') ? 'dist' : 'build';
     await execa('git', ['--work-tree', folderName, 'add', '--all']);
